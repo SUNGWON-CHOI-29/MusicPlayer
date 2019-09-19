2753980028cc11a4a68c62d264d871313cc0bf7d
SUNGWON-CHOI-29 <choi.sungwon.179.70@gmail.com>
2019-09-19 14:32:02 +0900

    Initial Commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { TouchableWithoutFeedback, StyleSheet, Text, View, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageButton: {
+    width: 200,
+    height: 200,
+  },
+});
+
+class ImageButton extends Component {
+  _onPressButton = () => {
+    this.setState(previousState => (
+      {isSelected: !previousState.isSelected}
+      ))
+  }
+
+  state = { isSelected: false}
+  render() {
+    var playPauseImage = this.state.isSelected
+    ? require('./assets/img/button_pause.png')
+    : require('./assets/img/button_play.png')
+
+    return (
+      <TouchableWithoutFeedback onPress={this._onPressButton}>
+        <Image source={playPauseImage} style={styles.imageButton}/>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+export default class FirstView extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={styles.container}>
+        <ImageButton/>  
+      </View>
+    );
+  }
+}
