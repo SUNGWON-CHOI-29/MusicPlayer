809b0d0db92e8215fd7217abf31e350c02bbaa63
SUNGWON-CHOI-29 <choi.sungwon.179.70@gmail.com>
2019-09-19 17:09:22 +0900

    Add react-native-track-player fail import that library on App.js, it's because of that library don't support Expo and Expokit`

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { TouchableWithoutFeedback, StyleSheet, Text, View, Image } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+import { TouchableWithoutFeedback, StyleSheet, Text, View, Image} from 'react-native';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -14,20 +16,13 @@ const styles = StyleSheet.create({
 });
 
 class ImageButton extends Component {
-  _onPressButton = () => {
-    this.setState(previousState => (
-      {isSelected: !previousState.isSelected}
-      ))
-  }
-
-  state = { isSelected: false}
   render() {
-    var playPauseImage = this.state.isSelected
+    var playPauseImage = this.props.isSelected
     ? require('./assets/img/button_pause.png')
     : require('./assets/img/button_play.png')
 
     return (
-      <TouchableWithoutFeedback onPress={this._onPressButton}>
+      <TouchableWithoutFeedback onPress={this.props.onPressButton}>
         <Image source={playPauseImage} style={styles.imageButton}/>
       </TouchableWithoutFeedback>
     );
@@ -35,13 +30,20 @@ class ImageButton extends Component {
 }
 
 export default class FirstView extends Component {
+  state = { isSelected: false}
+
+  _onPressButtonFromParent = () => {
+     this.setState(previousState => (
+       {isSelected: !previousState.isSelected}
+       ))
+  }
   render() {
     return (
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
       // What if you add `height: 300` instead of `flex: 1`?
       <View style={styles.container}>
-        <ImageButton/>  
+        <ImageButton onPressButton= {()=> this._onPressButtonFromParent()} isSelected= {this.state.isSelected} />
       </View>
     );
   }
